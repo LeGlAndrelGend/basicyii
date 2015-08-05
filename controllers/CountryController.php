6733367647ec3cf836de35a18e8dd7f0c823a202
LeGlAndrelGend <andrei.kalachev@gmail.com>
2015-08-05 17:18:49 +0300

    Created CRUD for table country

--- controllers/CountryController.php
+++ controllers/CountryController.php
@@ -9,13 +9,31 @@
 namespace app\controllers;
 
 
+use app\models\Country;
+use yii\data\Pagination;
 use yii\web\Controller;
 
 class CountryController extends Controller{
 
     public function actionIndex()
     {
-        return $this->render('index');
+//        return $this->render('index');
+        $query = Country::find();
+
+        $pagination = new Pagination([
+            'defaultPageSize' => 7,
+            'totalCount' => $query->count(),
+        ]);
+
+        $countries = $query->orderBy('name')
+            ->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+
+        return $this->render('index', [
+            'countries' => $countries,
+            'pagination' => $pagination,
+        ]);
     }
 
 }
